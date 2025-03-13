a29ec35830d49f11de909714b56731539099b61d
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-03-13 17:10:45 +0100

    lista di post recuperata dal database

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,7 +20,13 @@ function index(req,res){
 
     const sql = 'SELECT * FROM posts'
 
+    connection.query( sql,(err, results) => {
+        if(err) return res.status(500).json({
+            error: 'database query error'
+        })
 
+        res.json(results)
+    })
 }
 function show(req,res){
 
